10c18cdbc3bb595c5e655ec9dafb7b14ebf2bf79
Grant Heffernan <heffergm@gmail.com>
2016-09-02 08:52:00 -0400

    - move start command to script - update install to make generation of test data part of start,   otherwise assume data available on mounted filesystem

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 if [ ${INSTALL_FROM} = 'ppa' ]; then
-  /tmp/install_from_ppa.sh
+  /scripts/install_from_ppa.sh
 elif [ ${INSTALL_FROM} = 'source' ]; then
-  /tmp/install_from_source.sh
+  /scripts/install_from_source.sh
 else
   echo "Fail... INSTALL_FROM should be one of 'ppa' or 'source'."
   exit 1
---
+++ scripts/start_valhalla.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PATH=${PATH}:/usr/local/bin
+
+if [ ${GENERATE_TEST_DATA} = 'true' ]; then
+  echo "`date`: generating test data." >/tmp/start_valhalla.log 2>&1
+  wget -q https://s3.amazonaws.com/metro-extracts.mapzen.com/rome_italy.osm.pbf
+  mkdir -p /data/valhalla
+  valhalla_build_admins -c conf/valhalla.json *.pbf
+  valhalla_build_tiles -c conf/valhalla.json *.pbf
+  echo "`date`: done generating test data." >/tmp/start_valhalla.log 2>&1
+fi
+
+echo "`date`: starting route service." >/tmp/start_valhalla.log 2>&1
+if [ ${INSTALL_FROM} = 'ppa' ]; then
+  valhalla_route_service conf/valhalla.json
+elif [ ${INSTALL_FROM} = 'source' ]; then
+  tools/valhalla_route_service conf/valhalla.json
+fi
