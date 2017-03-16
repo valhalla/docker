55baa53aeec2d58bb927f3414c09299d9b2474b4
Grant Heffernan <heffergm@gmail.com>
2017-03-16 07:09:28 -0400

    lint

--- scripts/install_from_source.sh
+++ scripts/install_from_source.sh
@@ -8,17 +8,49 @@ if [[ $(grep -cF trusty /etc/lsb-release) > 0 ]]; then
   add-apt-repository -y ppa:kevinkreiser/zeromq3
   add-apt-repository -y ppa:kevinkreiser/czmq
 fi
+
 add-apt-repository -y ppa:kevinkreiser/prime-server
 apt-get update -y
 
 # get all the dependencies might need
-apt-get install -y git autoconf automake make libtool pkg-config g++ gcc jq lcov protobuf-compiler vim-common libboost-all-dev libboost-all-dev libcurl4-openssl-dev libprime-server0.6.3-dev libprotobuf-dev prime-server0.6.3-bin libgeos-dev libgeos++-dev liblua5.2-dev libspatialite-dev libsqlite3-dev lua5.2 python-all-dev
+apt-get install -y \
+  git \
+  autoconf \
+  automake \
+  make \
+  libtool \
+  pkg-config \
+  g++ \
+  gcc \
+  jq \
+  lcov \
+  protobuf-compiler \
+  vim-common \
+  libboost-all-dev \
+  libboost-all-dev \
+  libcurl4-openssl-dev \
+  libprime-server0.6.3-dev \
+  libprotobuf-dev prime-server0.6.3-bin \
+  libgeos-dev \
+  libgeos++-dev \
+  liblua5.2-dev \
+  libspatialite-dev \
+  libsqlite3-dev \
+  lua5.2 \
+  python-all-dev
+
 if [[ $(grep -cF xenial /etc/lsb-release) > 0 ]]; then
   apt-get install -y libsqlite3-mod-spatialite
 fi
 
 # get the software installed
-git clone --depth=1 --recurse-submodules --single-branch --branch=master https://github.com/valhalla/valhalla.git libvalhalla
+git clone \
+  --depth=1 \
+  --recurse-submodules \
+  --single-branch \
+  --branch=master \
+  https://github.com/valhalla/valhalla.git libvalhalla
+
 cd libvalhalla
 ./autogen.sh
 ./configure --enable-static
