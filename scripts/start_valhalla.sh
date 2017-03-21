668fac8db4cdf0eeaa6419beb95acdd2f0b7da1a
gknisely <greg@mapzen.com>
2017-03-21 10:37:17 -0400

    added interrupt env var for prime server.  it is required.

--- scripts/start_valhalla.sh
+++ scripts/start_valhalla.sh
@@ -19,5 +19,6 @@ export TYR_PROXY_OUT=$(jq -r ".tyr.service.proxy" conf/valhalla.json)_out
 export PRIME_LISTEN=$(jq -r ".httpd.service.listen" conf/valhalla.json)
 export PRIME_PROXY=$(jq -r ".loki.service.proxy" conf/valhalla.json)_in
 export PRIME_LOOPBACK=$(jq -r ".httpd.service.loopback" conf/valhalla.json)
+export PRIME_INTERRUPT=$(jq -r ".httpd.service.interrupt" conf/valhalla.json)
 
 /usr/bin/supervisord -n -c /conf/supervisord.conf
