90e69c17d346ff0fd1febf142cf079dd84df7363
gknisely <greg@mapzen.com>
2017-03-22 10:30:25 -0400

    add trusty support.

--- scripts/install_from_ppa.sh
+++ scripts/install_from_ppa.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# get all the ppas we might night
+if [[ $(grep -cF trusty /etc/lsb-release) > 0 ]]; then
+  add-apt-repository -y ppa:kevinkreiser/libsodium
+  add-apt-repository -y ppa:kevinkreiser/libpgm
+  add-apt-repository -y ppa:kevinkreiser/zeromq3
+  add-apt-repository -y ppa:kevinkreiser/czmq
+fi
+
 apt-add-repository -y ppa:kevinkreiser/prime-server
 apt-add-repository -y ppa:valhalla-routing/valhalla
 apt-get update -y
