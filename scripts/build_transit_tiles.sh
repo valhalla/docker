108d545132e2699ad4d98cae578557f06682c4ed
Grant Heffernan <heffergm@gmail.com>
2017-03-24 06:01:31 -0400

    build_transit script improved logging

--- scripts/build_transit_tiles.sh
+++ scripts/build_transit_tiles.sh
@@ -3,7 +3,7 @@ set -e
 
 # this has to exist
 if [ -z "${TRANSITLAND_API_KEY}" ]; then
-  echo "Environment variable TRANSITLAND_API_KEY is not set. Exiting."
+  echo "[ERROR] Environment variable TRANSITLAND_API_KEY is not set. Exiting."
   exit 1
 fi
 
@@ -19,11 +19,11 @@ export TRANSITLAND_LEVELS=${TRANSITLAND_LEVELS:-"4"}
 mkdir -p ${TRANSIT_TILE_DIR}
 
 # for now....build the timezones.
-echo -e "Building timezones... \c"
+echo -e "[INFO] Building timezones... \c"
 valhalla_build_timezones conf/valhalla.json
 
 # build transit tiles
-echo -e "Building tiles... \c"
+echo -e "[INFO] Building tiles... \c"
 valhalla_build_transit \
   conf/valhalla.json \
   ${TRANSITLAND_URL} \
@@ -34,7 +34,7 @@ valhalla_build_transit \
   ${TRANSITLAND_FEED} \
   ${TRANSIT_TEST_FILE}
 
-echo "done!"
+echo "[SUCCESS] valhalla_build_transit completed!"
 
 # time_stamp
 stamp=$(date +%Y_%m_%d-%H_%M_%S)
@@ -49,5 +49,5 @@ if  [ -n "$TRANSIT_S3_PATH" ]; then
 fi
 
 # cya
-echo "Run complete, exiting."
+echo "[SUCCESS] Run complete, exiting."
 exit 0
