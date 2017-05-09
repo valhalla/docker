89cb6bfc59b90b9e3c48aa661a172d70a7ac1750
Kevin Kreiser <kevinkreiser@gmail.com>
2017-05-09 09:07:24 -0400

    all deps in same place

--- scripts/install_from_ppa.sh
+++ scripts/install_from_ppa.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# get all the ppas we might night
-if [[ $(grep -cF trusty /etc/lsb-release) > 0 ]]; then
-  add-apt-repository -y ppa:kevinkreiser/libsodium
-  add-apt-repository -y ppa:kevinkreiser/libpgm
-  add-apt-repository -y ppa:kevinkreiser/zeromq3
-  add-apt-repository -y ppa:kevinkreiser/czmq
-fi
-
-apt-add-repository -y ppa:kevinkreiser/prime-server
-apt-add-repository -y ppa:valhalla-routing/valhalla
+# get all the ppas we might need
+apt-add-repository -y ppa:valhalla-core/valhalla
 apt-get update -y
 
 apt-get install -y \
