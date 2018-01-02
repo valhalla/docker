ef427c356b41ca815e8192c5c06f95bffa1436a6
Grant Heffernan <heffergm@gmail.com>
2018-01-02 17:16:48 -0500

    update run.sh to take an optional COMMAND override

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 usage() {
-  echo "Usage: ${0} [ppa|source] [tag]"
+  echo "Usage: ${0} [ppa|source] [tag] [cmd]"
   exit 1
 }
 
@@ -12,13 +12,23 @@ fi
 if [ ${1} == "ppa" ] || [ ${1} == "source" ]; then
   run=${1}
   tag=${2}
+  cmd=${3}
 else
   usage
 fi
 
-docker run \
-  --name valhalla \
-  -d \
-  -h valhalla \
-  -p 127.0.0.1:8002:8002 \
-  valhalla/docker:${run}-${tag}
+if [ -z ${cmd} ]; then
+  docker run \
+    --name valhalla \
+    -d \
+    -h valhalla \
+    -p 127.0.0.1:8002:8002 \
+    valhalla/docker:${run}-${tag}
+else
+  docker run \
+    --name valhalla \
+    -d \
+    -h valhalla \
+    -p 127.0.0.1:8002:8002 \
+    valhalla/docker:${run}-${tag} ${cmd}
+fi
