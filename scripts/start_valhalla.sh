6d84171b1ded94f36342d0fa7025a0edb2256ef1
Kevin Kreiser <kevinkreiser@gmail.com>
2018-01-23 09:20:27 -0500

    there is no more tyr as a separate binary

--- scripts/start_valhalla.sh
+++ scripts/start_valhalla.sh
@@ -13,9 +13,6 @@ export ODIN_PROXY_OUT=$(jq -r ".odin.service.proxy" conf/valhalla.json)_out
 export THOR_PROXY_IN=$(jq -r ".thor.service.proxy" conf/valhalla.json)_in
 export THOR_PROXY_OUT=$(jq -r ".thor.service.proxy" conf/valhalla.json)_out
 
-export TYR_PROXY_IN=$(jq -r ".tyr.service.proxy" conf/valhalla.json)_in
-export TYR_PROXY_OUT=$(jq -r ".tyr.service.proxy" conf/valhalla.json)_out
-
 export PRIME_LISTEN=$(jq -r ".httpd.service.listen" conf/valhalla.json)
 export PRIME_PROXY=$(jq -r ".loki.service.proxy" conf/valhalla.json)_in
 export PRIME_LOOPBACK=$(jq -r ".httpd.service.loopback" conf/valhalla.json)
